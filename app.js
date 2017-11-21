143eb73e8d555fe344a13e66355584a97196f2e2
afrika.williams@gmail.com <afrika.williams@gmail.com>
2017-11-21 23:55:16 +0000

    automatically backed up by learn

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 var express = require('express')
+const SiteController = require('./controllers/SiteControler.js')
 var app = express()
 
 // Load Controllers
 
+
+app.get ("/", SiteController.index)
+app.get ("/about", SiteController.about)
+app.get ("/other", SiteController.other)
 
 // Routes
 
