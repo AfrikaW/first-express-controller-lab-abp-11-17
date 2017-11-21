64afe044da8cf6ac1aff9d459da3b46ef52405c1
afrika.williams@gmail.com <afrika.williams@gmail.com>
2017-11-21 23:45:44 +0000

    automatically backed up by learn

--- controllers/SiteController.js
+++ controllers/SiteController.js
@@ -1,4 +1,24 @@
 const SiteController = {};
 
+app.get ("/", function (req, resp){
+  const welcomeText = "welcome to Afrika's Html"
+  const favoriteThings = ["reading", "coding", "fighting","teaching", "dancing"]
+  const viewData = {
+    "welcomeText": welcomeText,
+    name: "Afrika",
+    school: "Flatiron School",
+    colors: ["Red", "Yellow", "Blue"],
+    favoriteThings: favoriteThings
+  }
+  resp.render("index", viewData)
+  })
+}
+app.get ("/about", function (req, resp){
+  resp.render ("about")
+}
+app.get ("/other", function (req, resp){
+}
+
+
 
 module.exports = SiteController;
